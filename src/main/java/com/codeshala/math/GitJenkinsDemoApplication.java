fc233252abde001554987dc5b55bd07362438468
ShubhamGogulwar <sgogulwar@gmail.com>
2021-07-18 18:02:25 +0530

    Initial Commit

---
+++ src/main/java/com/codeshala/math/GitJenkinsDemoApplication.java
@@ -0,0 +1,14 @@
+package com.codeshala.math;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitJenkinsDemoApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		SpringApplication.run(GitJenkinsDemoApplication.class, args);
+	}
+
+}
